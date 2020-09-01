a969eee51eeeae949e14321da52924f5f3054432
wr0b1n <robin-lutter@gmx.de>
2020-09-01 09:51:53 +0200

    Books Create / Index / New

--- routes/books.js
+++ routes/books.js
@@ -18,7 +18,25 @@ const upload = multer({
 
 // All books route
 router.get('/', async (req, res) => {
-    res.send('All books')
+    let query = Book.find()
+    if(req.query.title != null && req.query.title != '') {
+        query = query.regex('title', new RegExp(req.query.title, 'i'))
+    }
+    if(req.query.publishedBefore != null && req.query.publishedBefore != '') {
+        query = query.lte('publishDate', req.query.publishedBefore)
+    }
+    if(req.query.publishedAfter != null && req.query.publishedAfter != '') {
+        query = query.gte('publishDate', req.query.publishedAfter)
+    }
+    try {
+        const books = await query.exec()
+        res.render('books/index', {
+            books: books,
+            searchOptions: req.query
+        })
+    } catch {
+        res.redirect('/')
+    }
 })
 
 // New book route
